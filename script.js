7a473fb440dd5703796e02fff75d6b2921bc73b3
manjuapps22 <81699226+manjuapps22@users.noreply.github.com>
2023-01-07 01:11:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Hello, government of the United States!');
+document.querySelector('header nav a').addEventListener('click', function(event) {
+  event.preventDefault();
+  alert('Sorry, this feature is not yet implemented.');
+});
